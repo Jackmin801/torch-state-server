eb56c81fae19ee65c3570640c391b8a91e2a9157
Jackmin801 <ongjackm@gmail.com>
2024-11-15 12:30:23 +0700

    refactor packing and impl float

--- torchstate/client.py
+++ torchstate/client.py
@@ -31,6 +31,14 @@ TTYPE_TO_ELEMENT_SIZE = {
     TransferType.UNIFORM_INT8.value: 1,
 }
 
+def _pack_request(path: str, transfer_type: int, size: int) -> bytes:
+    # Pack the request
+    if len(path) > 244:
+        raise ValueError("Path length must be at most 252 characters")
+    encoded_path = path.encode().ljust(244, b'\x00')
+    packed_request = struct.pack('244siq', encoded_path, transfer_type, size)
+    return packed_request
+
 class StateClient:
     def __init__(self, url: str):
         self.hostname, port = url.split(":")
@@ -44,18 +52,15 @@ class StateClient:
         inplace_tensor: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         # Pack the request
-        if len(path) > 244:
-            raise ValueError("Path length must be at most 252 characters")
-        encoded_path = path.encode().ljust(244, b'\x00')
         encoded_transfer_type = transfer_type.value if transfer_type else -1
         encoded_size = inplace_tensor.numel() if inplace_tensor else -1
-        packed_request = struct.pack('244siq', encoded_path, encoded_transfer_type, encoded_size)
+        packed_request = _pack_request(path, encoded_transfer_type, encoded_size)
 
         # Send packed request to the server
         self.client_socket.connect((self.hostname, self.port))
         self.client_socket.sendall(packed_request)
 
-        # Unpack the response
+        # Unpack the header
         if inplace_tensor is None:
             tensor_meta = self.client_socket.recv(64)
             tensor_meta = struct.unpack('iiqiiiiiiiiiiii', tensor_meta)
@@ -73,8 +78,10 @@ class StateClient:
         if succ != 0:
             if ttype == ScalarTransferType.STR.value:
                 response = self.client_socket.recv(size)
+                self.client_socket.close()
                 raise StateClientError(response.decode())
             else:
+                self.client_socket.close()
                 raise StateClientError("Failed to get tensor")
 
         # Get Codebook
@@ -91,7 +98,30 @@ class StateClient:
         self.client_socket.close()
     
     def get_float(self, path: str) -> float:
-        pass
+        # Pack the request
+        packed_request = _pack_request(path, ScalarTransferType.FLOAT64, 8)
+
+        # Send packed request to the server
+        self.client_socket.connect((self.hostname, self.port))
+        self.client_socket.sendall(packed_request)
+
+        # Unpack the header
+        resp_header = self.client_socket.recv(16)
+        succ, ttype, size = struct.unpack('iiq', resp_header)
+    
+        # Server error response
+        if succ != 0:
+            if ttype == ScalarTransferType.STR.value:
+                response = self.client_socket.recv(size)
+                raise StateClientError(response.decode())
+            else:
+                raise StateClientError("Failed to get float")
+        
+        # Receive the float data
+        float_data = self.client_socket.recv(8)
+        self.client_socket.close()
+        return struct.unpack('d', float_data)[0]
+
 
     def get_int(self, path: str) -> int:
         pass
